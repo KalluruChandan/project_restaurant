2d2511019f2e95dfe4e49bc4e0b2e7f9ac206a21
chandan <chandankalluri9@gmail.com>
2024-12-08 23:33:09 +0530

    test push for after restricting direct push on this branch(test-4)

--- src/main/java/com/project/resto/RestoApplication.java
+++ src/main/java/com/project/resto/RestoApplication.java
@@ -11,7 +11,7 @@ public class RestoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestoApplication.class, args);
-		log.info(Constants.APPLICATION_START);//test-3
+		log.info(Constants.APPLICATION_START);//test-4
 	}
 
 }
