7b99b5eb95a48415037e389443954bae2b90820a
chandan <chandankalluri9@gmail.com>
2024-12-08 22:37:04 +0530

    Initial commit

---
+++ src/main/java/com/project/resto/RestoApplication.java
@@ -0,0 +1,18 @@
+package com.project.resto;
+
+import com.project.resto.app.util.Constants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class RestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoApplication.class, args);
+		log.info(Constants.APPLICATION_START);
+	}
+
+}
+
---
+++ src/main/java/com/project/resto/app/util/Constants.java
@@ -0,0 +1,5 @@
+package com.project.resto.app.util;
+
+public class Constants {
+    public static final String APPLICATION_START = "------------------APPLICATION STARTED SUCCESSFULLY---------------------------";
+}
